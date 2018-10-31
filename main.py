c59f949dc5e1b4d7ddcfd4816d98347dd7882a29
aniket <adidolkar123@gmail.com>
2018-11-01 01:59:42 +0530

    change loss

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ def train():
         optimizer.param_groups[0]['lr'] = lr2 * seq_len / args.bptt
         model.train()
         data, targets = train_data.getitem(i,1)
-        targets=torch.from_numpy(targets).cuda()
+        targets=targets.cuda()
         # Starting each batch, we detach the hidden state from how it was previously produced.
         # If we didn't, the model would try backpropagating all the way to start of the dataset.
         hidden = repackage_hidden(hidden)
