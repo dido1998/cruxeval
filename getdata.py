c4b3c53370e5f3a4f1be4c42f603430fa92d3436
aniket <adidolkar123@gmail.com>
2018-10-31 18:09:10 +0530

    fixing embed matrix error

--- getdata.py
+++ getdata.py
@@ -84,7 +84,7 @@ class Vocab(object):
         if max_size != 0 and self._count >= max_size:
           print ("max_size of vocab was specified as %i; we now have %i words. Stopping reading." % (max_size, self._count))
           break
-    self.embed_matrix=np.zeros(len(self.embed_matrix_list),300)
+    self.embed_matrix=np.zeros((len(self.embed_matrix_list),300))
     for e,f in enumerate(self.embed_matrix_list):
       self.embed_matrix[e,:]=self.embed_matrix_list[e]
 
