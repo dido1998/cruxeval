8d8dab3c175b37b1fc3f07f3244cd6aedcda9a6b
aniket <adidolkar123@gmail.com>
2018-11-01 00:39:39 +0530

    check code

--- model.py
+++ model.py
@@ -74,8 +74,8 @@ class RNNModel(nn.Module):
     def forward(self, input, hidden, return_h=False):
         #emb = embedded_dropout(self.encoder, input, dropout=self.dropoute if self.training else 0)
         #emb = self.idrop(emb)
-
-        emb = self.lockdrop(input, self.dropouti)
+        emb=self.encoder(input)
+        emb = self.lockdrop(emb, self.dropouti)
 
         raw_output = emb
         new_hidden = []
