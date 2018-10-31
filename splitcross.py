a6717d181dd2dafdc305a5f52c37f48614668fb2
aniket <adidolkar123@gmail.com>
2018-11-01 02:46:56 +0530

    change loss

--- splitcross.py
+++ splitcross.py
@@ -112,9 +112,10 @@ class SplitCrossEntropyLoss(nn.Module):
             print()
 
         total_loss = None
-        #if len(hiddens.size()) > 2: hiddens = hiddens.view(-1, hiddens.size(2))
+        if len(hiddens.size()) > 2: hiddens = hiddens.view(-1, hiddens.size(2))
         print(hiddens.size())
         print(targets.size())
+        targets=targets.view(-1)
         split_targets, split_hiddens = self.split_on_targets(hiddens, targets)
 
         # First we perform the first softmax on the head vocabulary and the tombstones
