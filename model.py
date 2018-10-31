0768f11b466d20845fbe7be0ba8e52c98eda58ee
aniket <adidolkar123@gmail.com>
2018-11-01 01:03:57 +0530

    fix small errors

--- model.py
+++ model.py
@@ -72,11 +72,12 @@ class RNNModel(nn.Module):
         self.decoder.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, input, hidden, return_h=False):
-        #emb = embedded_dropout(self.encoder, input, dropout=self.dropoute if self.training else 0)
-        #emb = self.idrop(emb)
-        print(input.type())
+        #print(input.type())
         input=input.long().cuda()
-        emb=self.encoder(input)
+        emb = embedded_dropout(self.encoder, input, dropout=self.dropoute if self.training else 0)
+        #emb = self.idrop(emb)
+        
+        #emb=self.encoder(input)
         emb = self.lockdrop(emb, self.dropouti)
 
         raw_output = emb
