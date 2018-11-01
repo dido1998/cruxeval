bbb8753098edaa8b286412e4f9c742d651bc501e
aniket <adidolkar123@gmail.com>
2018-11-01 17:28:04 +0530

    fix input

--- main.py
+++ main.py
@@ -214,15 +214,16 @@ def train():
 
         total_loss += raw_loss.data
         optimizer.param_groups[0]['lr'] = lr2
-        if batch % args.log_interval == 0 and batch > 0:
-            cur_loss = total_loss.item() / args.log_interval
-            elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:05.5f} | ms/batch {:5.2f} | '
-                    'loss {:5.2f} | ppl {:8.2f} | bpc {:8.3f}'.format(
-                epoch, batch, len(train_data) // args.bptt, optimizer.param_groups[0]['lr'],
-                elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss), cur_loss / math.log(2)))
-            total_loss = 0
-            start_time = time.time()
+        #if batch % args.log_interval == 0 and batch > 0:
+        cur_loss = total_loss.item() / args.log_interval
+        elapsed = time.time() - start_time
+        print('| epoch {:3d} | {:5d}/{:5d} batches  | '
+                'loss {:5.2f} | ppl {:8.2f} | bpc {:8.3f}'.format(
+            epoch, batch,
+            elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss), cur_loss / math.log(2)))
+        total_loss = 0
+        start_time = time.time()
+        model_save(args.save)
         ###
         batch += 1
         i += seq_len
