0400ced102555deca8c3f8ee3f7e5a9f2d1033f5
aniket <adidolkar123@gmail.com>
2018-11-01 19:30:04 +0530

    all working hopefully

--- main.py
+++ main.py
@@ -217,6 +217,7 @@ def train():
         if batch % args.loginterval == 0 and batch > 0:
             cur_loss = total_loss.item() / args.loginterval
             elapsed = time.time() - start_time
+            torch.save(model.state_dict(), args.save)
             print('| epoch {:3d} | {:5f}/{:5f} batches  | '
                     'loss {:5.2f} | ppl {:8.2f} | bpc {:8.3f}'.format(
                 epoch, batch,
@@ -224,7 +225,7 @@ def train():
             total_loss = 0
             start_time = time.time()
             #torch.save(d.state_dict(),'model/dis')
-            torch.save(model.state_dict(), args.save)
+            
             ###
         batch += 1
         
