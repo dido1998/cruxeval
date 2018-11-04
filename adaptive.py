15e9623ab22d2f0d60ca78c964547f71043df41c
aniket <adidolkar123@gmail.com>
2018-11-05 00:41:39 +0530

    change loss

--- adaptive.py
+++ adaptive.py
@@ -180,6 +180,7 @@ class AdaptiveLogSoftmaxWithLoss(Module):
                 gather_inds.index_fill_(0, row_indices, cluster_index)
 
                 cluster_logprob = log_softmax(cluster_output, dim=1)
+                relative_target=relative_target.long().cuda()
                 local_logprob = cluster_logprob.gather(1, relative_target.unsqueeze(1))
                 output.index_copy_(0, row_indices, local_logprob.squeeze(1))
 
