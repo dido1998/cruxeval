1bc4c1b49c6748a51c0a8edfedf5a4e690862a82
aniket <adidolkar123@gmail.com>
2018-11-04 18:53:31 +0530

    fix generate

--- generate.py
+++ generate.py
@@ -91,10 +91,13 @@ if args.cuda:
 with open(args.outf, 'w') as outf:
     for i in range(args.words):
         output, hidden = model(input, hidden)
+        print(output.size())
+        print(hidden.size())
+        print('---------------------------')
         word_weights = output.squeeze().data.div(args.temperature).exp().cpu()
         word_idx = torch.multinomial(word_weights, 1)[0]
         input.data.fill_(word_idx)
-        print(word_idx)
+        #print(word_idx)
         word = vocab_obj.id2word(word_idx.item())
 
         outf.write(word + ('\n' if i % 20 == 19 else ' '))
