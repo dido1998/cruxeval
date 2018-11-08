301fa38b96e7b07c1b5c23c519a1917c22001e4d
aniket <adidolkar123@gmail.com>
2018-11-08 22:21:50 +0530

    sanity check

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ if args.resume:
             elif rnn.zoneout > 0: rnn.zoneout = args.wdrop
 ###
 if not criterion:
-    splits = [50000]
+    splits = []
     if ntokens > 500000:
         # One Billion
         # This produces fairly even matrix mults for the buckets:
